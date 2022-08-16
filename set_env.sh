4476ad8d9ce6535e239374a5936f5688d88174e1
Ahmed Yasser <ahmed_m_yasser@hotmail.com>
2022-08-17 00:51:19 +0200

    Project init

---
+++ set_env.sh
@@ -0,0 +1,13 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+# mydbinstance.csxbuclmtj3c.us-east-1.rds.amazonaws.com
+export POSTGRES_USERNAME=postgres
+export POSTGRES_PASSWORD=Youshop@123
+export POSTGRES_HOST=udagram
+export POSTGRES_DB=postgres
+export DB_PORT=5000
+export AWS_BUCKET=arn:aws:s3:::myawsbucket-75139724085
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=mysecretstring
+export URL=http://localhost:8100
